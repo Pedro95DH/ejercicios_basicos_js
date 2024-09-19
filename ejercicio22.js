d1c7eca223c314e70b12dafddf0a95159814295c
Pedro95DH <pedrosdhdez@gmail.com>
2024-09-19 15:44:12 +0100

    Ejercicios terminados
    Ejercicios terminados

---
+++ ejercicio22.js
@@ -0,0 +1,30 @@
+// Usa un for para remplazar todas las comidas que no sean veganas con las frutas del array de frutas.
+
+// Recuerda no usar frutas duplicadas.
+
+// Finalmente, imprime el array resultante.
+
+const fruits = ["Strawberry", "Banana", "Orange", "Apple"];
+
+const foodSchedule = [
+  { name: "Heura", isVegan: true },
+  { name: "Salmon", isVegan: false },
+  { name: "Tofu", isVegan: true },
+  { name: "Burger", isVegan: false },
+  { name: "Rice", isVegan: true },
+  { name: "Pasta", isVegan: true },
+];
+
+const newArray = [];
+
+
+for (let i = 0; i < foodSchedule.length; i++) {
+    if(foodSchedule[i].isVegan){
+        newArray.push(foodSchedule[i]);
+    } else{
+        newArray.push({name: fruits[i],isVegan: true})
+    }
+}
+
+console.log(newArray);
+
